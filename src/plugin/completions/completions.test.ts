5ee60b580d99b65fcdd40a684dabeb8e8e6ec93b
Matt Walker <matt.walker@genesis.global>
2023-05-10 11:19:17 +0100

    test: unit tests for getting completion type FUI-1189

--- src/plugin/completions/completions.test.ts
+++ src/plugin/completions/completions.test.ts
@@ -1,3 +1,5 @@
+import { TemplateContext } from "typescript-template-language-service-decorator";
+import { LineAndCharacter } from "typescript/lib/tsserverlibrary";
 import { buildDefaultCEFake } from "../../jest/custom-elements-resource";
 import { getLogger, html } from "../../jest/utils";
 import { CustomElementsResource } from "../transformer/custom-elements-resource";
@@ -7,17 +9,95 @@ const getCompletionsService = (ceRes: CustomElementsResource) =>
   new CompletionsService(getLogger(), ceRes);
 
 describe("getCompletionType", () => {
-  it('Returns "none" if we are in a blank template', () => {
-    const service = getCompletionsService(buildDefaultCEFake());
-    const context = html``;
+  const tests: [string, [TemplateContext, LineAndCharacter], any][] = [
+    [
+      'Key "none" for a blank template',
+      [html``, { line: 0, character: 0 }],
+      { key: "none", params: undefined },
+    ],
+    [
+      'Key "none" after a fully closed custom element',
+      [html`<c-e></c-e>`, { line: 0, character: 11 }],
+      { key: "none", params: undefined },
+    ],
+    [
+      'Key "none" after a fully closed element',
+      [html`<div></div>`, { line: 0, character: 11 }],
+      { key: "none", params: undefined },
+    ],
+    [
+      'Key "none" after a self closing element',
+      [html`<input />`, { line: 0, character: 9 }],
+      { key: "none", params: undefined },
+    ],
+    [
+      'Key "none" after in invalid empty tag',
+      [html`<>`, { line: 0, character: 2 }],
+      { key: "none", params: undefined },
+    ],
+    [
+      'Key "none" after an unclosed tag',
+      [html`<div>`, { line: 0, character: 5 }],
+      { key: "none", params: undefined },
+    ],
+    // custom-element-name
+    [
+      'Key "custom-element-name" after an opening tag',
+      [html`<`, { line: 0, character: 1 }],
+      { key: "custom-element-name", params: undefined },
+    ],
+    [
+      'Key "custom-element-name" after an opening with characters',
+      [html`<di`, { line: 0, character: 3 }],
+      { key: "custom-element-name", params: undefined },
+    ],
+    [
+      'Key "custom-element-name" after an opening with characters, after a valid tag',
+      [html`<div></div><di`, { line: 0, character: 14 }],
+      { key: "custom-element-name", params: undefined },
+    ],
+    // custom-element-attribute, with custom element name
+    [
+      'Key "custom-element-attribute" after the start of a custom element, and the name',
+      [html`<cus-`, { line: 0, character: 5 }],
+      { key: "custom-element-attribute", params: "cus-" },
+    ],
+    [
+      'Key "custom-element-attribute" after a custom element, and the name',
+      [html`<cus-elem`, { line: 0, character: 9 }],
+      { key: "custom-element-attribute", params: "cus-elem" },
+    ],
+    [
+      'Key "custom-element-attribute" after a custom element accounting for another custom element',
+      [html`<ce-elem></ce-elem><cus-elem`, { line: 0, character: 28 }],
+      { key: "custom-element-attribute", params: "cus-elem" },
+    ],
+    [
+      'Key "custom-element-attribute" and gets custom element name when attribute is present',
+      [html`<ce-elem></ce-elem><cus-elem attr`, { line: 0, character: 33 }],
+      { key: "custom-element-attribute", params: "cus-elem" },
+    ],
+    [
+      'Key "custom-element-attribute" and gets custom element name when string attribute is present',
+      [html`<ce-elem></ce-elem><cus-elem attr="value"`, { line: 0, character: 41 }],
+      { key: "custom-element-attribute", params: "cus-elem" },
+    ],
+    [
+      'Key "custom-element-attribute" and gets custom element when curso is inside of a fully closed element',
+      [html`<ce-elem></ce-elem><cus-elem attr="value"></cus-elem>`, { line: 0, character: 41 }],
+      { key: "custom-element-attribute", params: "cus-elem" },
+    ],
+  ];
 
-    // `getComptionType` is private so need to case to `any` to test it
-    const result = (service as any).getComptionType(context, {
-      line: 0,
-      character: 0,
+  for (const test of tests) {
+    const [name, [context, lineAndChar], expected] = test;
+    it(name, () => {
+      const service = getCompletionsService(buildDefaultCEFake());
+      // `getComptionType` is private so need to case to `any` to test it
+      const result = (service as any).getComptionType(context, lineAndChar);
+      expect(result).toEqual(expected);
     });
-    expect(result).toEqual({ key: "none", params: undefined });
-  });
+  }
 });
 
 describe("getCompletionsAtPosition", () => {
