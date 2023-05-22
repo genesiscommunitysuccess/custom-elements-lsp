e1ba38b6f5af9916d4873b3adb4f2dad4b7f700c
Matt Walker <matt.walker@genesis.global>
2023-05-22 17:12:18 +0100

    test: fix tests after refactor FUI-1191

--- src/plugin/completions/completions.test.ts
+++ src/plugin/completions/completions.test.ts
@@ -1,11 +1,15 @@
 import { TemplateContext } from "typescript-template-language-service-decorator";
-import { LineAndCharacter } from "typescript/lib/tsserverlibrary";
+import {
+  CompletionInfo,
+  LineAndCharacter,
+} from "typescript/lib/tsserverlibrary";
 import { getCEServiceFromStubbedResource } from "../../jest/custom-elements";
 import { getGDServiceFromStubbedResource } from "../../jest/global-data";
 import { buildServices, getLogger, html } from "../../jest/utils";
 import { CustomElementsService } from "../custom-elements/custom-elements.types";
 import { GlobalDataService } from "../global-data/global-data.types";
 import { CoreCompletionsServiceImpl } from "./completions";
+import { getCompletionType } from "./helpers";
 
 const getCompletionsService = (
   ceRes: CustomElementsService = getCEServiceFromStubbedResource(),
@@ -49,112 +53,27 @@ const globalDataAttributeAssersions = [
   },
 ];
 
-describe("getCompletionType", () => {
-  const tests: [string, [TemplateContext, LineAndCharacter], any][] = [
-    [
-      'Key "none" for a blank template',
-      [html``, { line: 0, character: 0 }],
-      { key: "none", params: undefined },
-    ],
-    [
-      'Key "none" after a fully closed custom element',
-      [html`<c-e></c-e>`, { line: 0, character: 11 }],
-      { key: "none", params: undefined },
-    ],
-    [
-      'Key "none" after a fully closed element',
-      [html`<div></div>`, { line: 0, character: 11 }],
-      { key: "none", params: undefined },
-    ],
-    [
-      'Key "none" after a self closing element',
-      [html`<input />`, { line: 0, character: 9 }],
-      { key: "none", params: undefined },
-    ],
-    [
-      'Key "none" after in invalid empty tag',
-      [html`<>`, { line: 0, character: 2 }],
-      { key: "none", params: undefined },
-    ],
-    [
-      'Key "none" after an unclosed tag',
-      [html`<div></div>`, { line: 0, character: 5 }],
-      { key: "none", params: undefined },
-    ],
-    // custom-element-name
-    [
-      'Key "custom-element-name" after an opening tag',
-      [html`<`, { line: 0, character: 1 }],
-      { key: "custom-element-name", params: undefined },
-    ],
-    [
-      'Key "custom-element-name" after an opening with characters',
-      [html`<di`, { line: 0, character: 3 }],
-      { key: "custom-element-name", params: undefined },
-    ],
-    [
-      'Key "custom-element-name" after an opening with characters, after a valid tag',
-      [html`<div></div><di`, { line: 0, character: 14 }],
-      { key: "custom-element-name", params: undefined },
-    ],
-    // custom-element-attribute, with custom element name
-    [
-      'Key "custom-element-attribute" after the start of a custom element, and the name',
-      [html`<cus-`, { line: 0, character: 5 }],
-      { key: "custom-element-attribute", params: "cus-" },
-    ],
-    [
-      'Key "custom-element-attribute" after a custom element, and the name',
-      [html`<cus-elem`, { line: 0, character: 9 }],
-      { key: "custom-element-attribute", params: "cus-elem" },
-    ],
-    [
-      'Key "custom-element-attribute" after a custom element accounting for another custom element',
-      [html`<ce-elem></ce-elem><cus-elem`, { line: 0, character: 28 }],
-      { key: "custom-element-attribute", params: "cus-elem" },
-    ],
-    [
-      'Key "custom-element-attribute" and gets custom element name when attribute is present',
-      [html`<ce-elem></ce-elem><cus-elem attr`, { line: 0, character: 33 }],
-      { key: "custom-element-attribute", params: "cus-elem" },
-    ],
-    [
-      'Key "custom-element-attribute" and gets custom element name when string attribute is present',
-      [
-        html`<ce-elem></ce-elem><cus-elem attr="value"`,
-        { line: 0, character: 41 },
-      ],
-      { key: "custom-element-attribute", params: "cus-elem" },
-    ],
-    [
-      'Key "custom-element-attribute" and gets custom element when curso is inside of a fully closed element',
-      [
-        html`<ce-elem></ce-elem><cus-elem attr="value"></cus-elem>`,
-        { line: 0, character: 41 },
-      ],
-      { key: "custom-element-attribute", params: "cus-elem" },
-    ],
-  ];
-
-  for (const test of tests) {
-    const [name, [context, lineAndChar], expected] = test;
-    it(name, () => {
-      const service = getCompletionsService();
-      // `getComptionType` is private so need to case to `any` to test it
-      const result = (service as any).getComptionType(context, lineAndChar);
-      expect(result).toEqual(expected);
-    });
-  }
-});
+const baseCompletionInfo: CompletionInfo = {
+  isGlobalCompletion: false,
+  isMemberCompletion: false,
+  isNewIdentifierLocation: false,
+  entries: [],
+};
 
 describe("getCompletionsAtPosition", () => {
   it("Returns no completions if we are in a blank template", () => {
     const service = getCompletionsService();
     const context = html``;
-
-    const completions = service.getCompletionsAtPosition(context, {
+    const position = {
       line: 0,
       character: 0,
+    };
+    const typeAndParam = getCompletionType(context, position);
+
+    const completions = service.getCompletionsAtPosition(baseCompletionInfo, {
+      context,
+      position,
+      typeAndParam,
     });
 
     expect(completions.entries).toHaveLength(0);
@@ -163,10 +82,16 @@ describe("getCompletionsAtPosition", () => {
   it("Returns completions for the custom element tags if inside of an opening tag", () => {
     const service = getCompletionsService();
     const context = html`<`;
-
-    const completions = service.getCompletionsAtPosition(context, {
+    const position = {
       line: 0,
       character: 1,
+    };
+    const typeAndParam = getCompletionType(context, position);
+
+    const completions = service.getCompletionsAtPosition(baseCompletionInfo, {
+      context,
+      position,
+      typeAndParam,
     });
 
     expect(completions.entries).toEqual([
@@ -196,10 +121,16 @@ describe("getCompletionsAtPosition", () => {
   it("Returns completions for the custom element tags if inside of an opening with an incomplete custom element", () => {
     const service = getCompletionsService();
     const context = html`<custom-eleme`;
-
-    const completions = service.getCompletionsAtPosition(context, {
+    const position = {
       line: 0,
       character: 13,
+    };
+    const typeAndParam = getCompletionType(context, position);
+
+    const completions = service.getCompletionsAtPosition(baseCompletionInfo, {
+      context,
+      position,
+      typeAndParam,
     });
 
     expect(completions.entries).toEqual([
@@ -229,10 +160,16 @@ describe("getCompletionsAtPosition", () => {
   it("Returns attribute completions when past a valid custom element name which has defined attributes", () => {
     const service = getCompletionsService();
     const context = html`<custom-element `;
-
-    const completions = service.getCompletionsAtPosition(context, {
+    const position = {
       line: 0,
       character: 16,
+    };
+    const typeAndParam = getCompletionType(context, position);
+
+    const completions = service.getCompletionsAtPosition(baseCompletionInfo, {
+      context,
+      position,
+      typeAndParam,
     });
 
     expect(completions.entries).toEqual([
@@ -263,10 +200,16 @@ describe("getCompletionsAtPosition", () => {
   it("Returns attribute completions when writing an attribute", () => {
     const service = getCompletionsService();
     const context = html`<custom-element col`;
-
-    const completions = service.getCompletionsAtPosition(context, {
+    const position = {
       line: 0,
       character: 19,
+    };
+    const typeAndParam = getCompletionType(context, position);
+
+    const completions = service.getCompletionsAtPosition(baseCompletionInfo, {
+      context,
+      position,
+      typeAndParam,
     });
 
     expect(completions.entries).toEqual([
@@ -297,10 +240,16 @@ describe("getCompletionsAtPosition", () => {
   it("Returns attribute completions when writing an attribute inside of a finished tag", () => {
     const service = getCompletionsService();
     const context = html`<custom-element colour="red"></custom-element>`;
-
-    const completions = service.getCompletionsAtPosition(context, {
+    const position = {
       line: 0,
       character: 28,
+    };
+    const typeAndParam = getCompletionType(context, position);
+
+    const completions = service.getCompletionsAtPosition(baseCompletionInfo, {
+      context,
+      position,
+      typeAndParam,
     });
 
     expect(completions.entries).toEqual([
@@ -331,10 +280,16 @@ describe("getCompletionsAtPosition", () => {
   it("Returns only the global attribute completions when we try and complete attributes on a custom element with no attributes", () => {
     const service = getCompletionsService();
     const context = html`<no-attr `;
-
-    const completions = service.getCompletionsAtPosition(context, {
+    const position = {
       line: 0,
-      character: 9,
+      character: 28,
+    };
+    const typeAndParam = getCompletionType(context, position);
+
+    const completions = service.getCompletionsAtPosition(baseCompletionInfo, {
+      context,
+      position,
+      typeAndParam,
     });
 
     expect(completions.entries).toEqual([...globalDataAttributeAssersions]);
@@ -343,10 +298,16 @@ describe("getCompletionsAtPosition", () => {
   it("Returns name completions when we try and complete attributes on an unknown custom element", () => {
     const service = getCompletionsService();
     const context = html`<unknown-element `;
-
-    const completions = service.getCompletionsAtPosition(context, {
+    const position = {
       line: 0,
       character: 17,
+    };
+    const typeAndParam = getCompletionType(context, position);
+
+    const completions = service.getCompletionsAtPosition(baseCompletionInfo, {
+      context,
+      position,
+      typeAndParam,
     });
 
     expect(completions.entries).toEqual([
