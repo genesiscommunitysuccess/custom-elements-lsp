06f2d14f12c06ff064ca44cfbb703ba1df39587e
Matt Walker <matt.walker@genesis.global>
2023-06-09 15:08:50 +0100

    test: update tests for valid properties FUI-1193

--- src/plugin/diagnostics/fast.test.ts
+++ src/plugin/diagnostics/fast.test.ts
@@ -43,6 +43,34 @@ describe('filterValidAttributes', () => {
     expect(res).toEqual(true);
   });
 
+  it('returns true for a diagnostic which was for a valid member binding attribute', () => {
+    const service = getFASTDiagnosticsService(getCEServiceFromStubbedResource());
+    const validDiagnostic: Diagnostic = {
+      length: 1,
+      start: 1,
+      category: 1,
+      code: UNKNOWN_ATTRIBUTE,
+      messageText: 'Unknown attribute ":member" for custom element "custom-element"',
+      file,
+    };
+    const res = (service as any).filterValidAttributes(validDiagnostic);
+    expect(res).toBe(false);
+  });
+
+  it('returns false for a diagnostic which was for an member binding attribute', () => {
+    const service = getFASTDiagnosticsService(getCEServiceFromStubbedResource());
+    const input: Diagnostic = {
+      length: 1,
+      start: 1,
+      category: 0,
+      code: UNKNOWN_ATTRIBUTE,
+      messageText: 'Unknown attribute ":member" for custom element "no-attr"',
+      file,
+    };
+    const res = (service as any).filterValidAttributes(input);
+    expect(res).toEqual(true);
+  });
+
   it('returns false for other attr code types', () => {
     const service = getFASTDiagnosticsService(getCEServiceFromStubbedResource());
     const input: Diagnostic = {
