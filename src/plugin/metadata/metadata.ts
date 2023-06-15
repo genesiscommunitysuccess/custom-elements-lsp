8aab0eb08d5e72fbc476f9cd60c5af379ca499fd
Matt Walker <matt.walker@genesis.global>
2023-06-15 08:55:46 +0100

    feat: find the correct location of the token to jump to in dest FUI-1190

--- src/plugin/metadata/metadata.ts
+++ src/plugin/metadata/metadata.ts
@@ -42,7 +42,7 @@ export class CoreMetadataService implements MetadataService {
     );
 
     if (this.services.customElements.customElementKnown(token)) {
-      return this.getCustomElementDefinitionInfo(maybeTokenSpan, token);
+      return this.getCustomElementDefinitionInfo(maybeTokenSpan, token, context);
     }
 
     return {
@@ -52,21 +52,38 @@ export class CoreMetadataService implements MetadataService {
 
   private getCustomElementDefinitionInfo(
     tokenSpan: TextSpan,
-    tagName: string
+    tagName: string,
+    context: TemplateContext
   ): DefinitionInfoAndBoundSpan {
     const path = this.services.customElements.getCEPath(tagName, { getFullPath: true });
     if (!path) {
       throw new Error("Couldn't find path for custom element with tagName: " + path);
     }
 
+    const fileName = this.services.io.getNormalisedRootPath() + '/' + path;
+
+    // naive but simple approach to finding the definition location in the source file
+    const definitionStart = this.services.io.getLocationOfStringInFile(path, tagName);
+    if (!definitionStart) {
+      throw new Error(
+        `Couldn't find definition for custom element with tagName: ${tagName} defined in file ${fileName}`
+      );
+    }
+
+    // If we return the `textSpan` start of the definition as the location of the definition in the source file,
+    // this is wrong because the API then adds on the offset of the start of the *current* template
+    // string onto the start. So we need to manually get the node text start ourselves and subtract
+    // it to cancel out the offset.
+    const nodeTextStart = context.node.getStart();
+
     return {
       textSpan: tokenSpan,
       definitions: [
         {
-          textSpan: { start: 0, length: 0 },
-          kind: ScriptElementKind.unknown,
-          fileName: this.services.io.getNormalisedRootPath() + '/' + path,
-          containerKind: ScriptElementKind.unknown,
+          fileName,
+          textSpan: { start: definitionStart - nodeTextStart - 1, length: tagName.length },
+          kind: ScriptElementKind.classElement,
+          containerKind: ScriptElementKind.moduleElement,
           containerName: 'file',
           name: tagName,
         },
