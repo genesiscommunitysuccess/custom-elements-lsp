a54b5d598524e5333ece091b2ea46c2991058a18
Matt Walker <matt.walker@genesis.global>
2023-09-14 15:50:05 +0100

    plugin works with inheritance custom annotation FUI-1484

--- src/analyzer-import-alias-plugin/fixtures/default/sourcecode/default.js
+++ src/analyzer-import-alias-plugin/fixtures/default/sourcecode/default.js
@@ -2,6 +2,7 @@ import { MyElement as ParentElement } from './superclass';
 import { Foo } from '@blah';
 import { Curry as CurryNights, Prawn } from '@bar';
 
+/**
+ * @inheritance MyElement -> ChildElement
+ */
 export class MyElement extends ParentElement {}
-
-
--- src/analyzer-import-alias-plugin/index.js
+++ src/analyzer-import-alias-plugin/index.js
@@ -1,61 +1,39 @@
 const name = 'analyzer-import-alias-plugin';
 
-function cacheImportAlias(context, aliasName, baseName, path) {
-  if (context[name].alias === undefined) {
-    context[name].alias = new Map();
-  }
-  context[name].alias.set(`${path};${aliasName}`, baseName);
-}
-
-function readCacheImportAlias(context, aliasName, path) {
-  return context[name].alias.get(`${path};${aliasName}`);
-}
-
-function procesImport({ ts, node, moduleDoc, context }) {
-  // debugger;
-  node?.importClause?.namedBindings?.elements
-    ?.filter((x) => x.propertyName !== undefined)
-    .forEach((x) =>
-      cacheImportAlias(context, x.name.getText(), x.propertyName.getText(), moduleDoc.path)
-    );
-}
-
 export default function myPlugin() {
   return {
     name,
-    // Runs for all modules in a project, before continuing to the `analyzePhase`
-    collectPhase({ ts, node, context }) {
-      if (context[name] === undefined) {
-        context[name] = {};
-      }
-    },
+    collectPhase({ ts, node, context }) {},
     // Runs for each module
     analyzePhase({ ts, node, moduleDoc, context }) {
-      // debugger;
-      // console.log(ts.SyntaxKind[node.kind])
-      // context[name] = context[name] ? context[name] + 1 : 1;
       switch (node.kind) {
-        case ts.SyntaxKind.ImportDeclaration:
-          procesImport({ ts, node, moduleDoc, context });
+        case ts.SyntaxKind.ClassDeclaration:
+          const className = node.name.getText();
+          node?.jsDoc?.forEach((jsDoc) => {
+            jsDoc.tags?.forEach((tag) => {
+              if (tag.tagName.getText() === 'inheritance') {
+                const maybeMatches = [...tag.comment.match(/(\w+)\s+->\s+(\w+)/)];
+                if (maybeMatches.length === 3) {
+                  const [_, superclassName, subclassName] = maybeMatches;
+                  const classDeclaration = moduleDoc.declarations.find(
+                    (declaration) => declaration.name === className
+                  );
+                  const exportDeclaration = moduleDoc.exports.find((exp) => exp.name === className);
+                  debugger;
+                  if (classDeclaration?.superclass?.name && exportDeclaration.declaration?.name) {
+                    classDeclaration.superclass.name = superclassName;
+                    classDeclaration.name = subclassName;
+                    exportDeclaration.name = subclassName;
+                    exportDeclaration.declaration.name = subclassName;
+                  }
+                  debugger;
+                }
+              }
+            });
+          });
       }
     },
-    // Runs for each module, after analyzing, all information about your module should now be available
-    moduleLinkPhase({ moduleDoc, context }) {
-      moduleDoc.declarations
-        ?.filter((dec) => dec.kind === 'class')
-        .forEach((dec) => {
-          const maybeBaseName = readCacheImportAlias(context, dec.superclass?.name, moduleDoc.path);
-          if (maybeBaseName) {
-            dec.name = dec.name;
-            dec.superclass.name = maybeBaseName;
-          }
-        });
-      // debugger;
-    },
-    // Runs after modules have been parsed and after post-processing
-    packageLinkPhase({ customElementsManifest, context }) {
-      debugger;
-      console.log(context);
-    },
+    moduleLinkPhase({ moduleDoc, context }) {},
+    packageLinkPhase({ customElementsManifest, context }) {},
   };
 }
