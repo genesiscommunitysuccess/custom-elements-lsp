2de22c5d82292838b7ca6034d6cde6af4b12f07a
Matt Walker <matt.walker@genesis.global>
2023-10-12 15:33:46 +0100

    chore: seperate test tsconfig FUI-1512

--- packages/core/custom-elements-lsp/jest.config.js
+++ packages/core/custom-elements-lsp/jest.config.js
@@ -2,4 +2,12 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/?(*.)+(spec|test).[t]s?(x)'],
+  transform: {
+    '^.+\\.test.ts$': [
+      'ts-jest',
+      {
+        tsconfig: './src/tsconfig.jest.json',
+      },
+    ],
+  },
 };
