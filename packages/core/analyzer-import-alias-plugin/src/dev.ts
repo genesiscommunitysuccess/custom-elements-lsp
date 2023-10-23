0cd84bd5d6d93b1b6352cc7b330501d73fa81c67
Matt Walker <matt.walker@genesis.global>
2023-10-23 09:23:31 +0100

    fix: dependencies setup with typescript for analyzer plugin FUI-1528

--- packages/core/analyzer-import-alias-plugin/src/dev.ts
+++ packages/core/analyzer-import-alias-plugin/src/dev.ts
@@ -1,11 +1,23 @@
 import fs from 'fs';
 import ts from 'typescript';
+// import ts from '../../../../../../../../../../../tmp/cem-plugin-template/node_modules/typescript/lib/typescript.js';
 import { create } from '@custom-elements-manifest/analyzer/src/create.js';
 import myPlugin from './index.js';
 
+console.log(`ts.version: ${ts.version} in lib`);
+
 debugger;
 
-const code = fs.readFileSync('fixtures/default/sourcecode/default.js').toString();
+// const code = fs.readFileSync('fixtures/default/sourcecode/default.js').toString();
+const code = `
+export class MyElement extends HTMLElement {
+  /**
+   * @foo Some custom information!
+   */
+  someField = '';
+}
+`;
+console.log(code);
 
 const modules = [ts.createSourceFile('my-element.js', code, ts.ScriptTarget.ES2015, true)];
 
