83bda6cc4a7179cbce7ffadd0408b123c3cc4df5
Matt Walker <matt.walker@genesis.global>
2023-10-24 16:28:17 +0100

    feat: (broken) override the superclass and class name FUI-1528

--- packages/core/analyzer-import-alias-plugin/src/index.ts
+++ packages/core/analyzer-import-alias-plugin/src/index.ts
@@ -1,5 +1,5 @@
 import { Plugin } from '@custom-elements-manifest/analyzer';
-import type { ClassDeclaration } from 'custom-elements-manifest';
+import type { ClassDeclaration, Reference } from 'custom-elements-manifest';
 import * as ts from 'typescript';
 
 const pluginName = 'analyzer-import-alias-plugin';
@@ -14,6 +14,7 @@ export type ImportAliasPluginOptions = {
 };
 
 export default function importAliasPlugin(config: ImportAliasPluginOptions): Plugin {
+  const checkedModules = Object.keys(config);
   return {
     name: pluginName,
     // Runs for all modules in a project, before continuing to the `analyzePhase`
@@ -27,16 +28,42 @@ export default function importAliasPlugin(config: ImportAliasPluginOptions): Plu
         case ts.SyntaxKind.ClassDeclaration:
           const classDeclerationNode = node as ts.ClassDeclaration;
           const className = classDeclerationNode.name?.getText();
-          const maybeSuperclassName = (
-            moduleDoc.declarations?.find(
-              ({ name, kind }) => name === className && kind === 'class',
-            ) as ClassDeclaration | undefined
-          )?.superclass?.name;
+          const classDef = moduleDoc.declarations?.find(
+            ({ name, kind }) => name === className && kind === 'class',
+          ) as ClassDeclaration | undefined;
           debugger;
+          if (
+            classDef?.superclass?.package &&
+            checkedModules.includes(classDef?.superclass?.package)
+          ) {
+            processOverride(classDef, config);
+          }
       }
     },
     moduleLinkPhase({ moduleDoc, context }) {},
     // Runs after modules have been parsed and after post-processing
-    packageLinkPhase({ customElementsManifest, context }) {},
+    packageLinkPhase({ customElementsManifest, context }) {
+      // TODO: Can I set the names back here now?
+    },
   };
+}
+
+function processOverride(classDef: ClassDeclaration, config: ImportAliasPluginOptions): void {
+  debugger;
+  const { package: pkg, name } = classDef.superclass as Reference;
+  const importConfig = config[pkg as string];
+
+  const maybeOverrideName = importConfig.override?.[name];
+  if (maybeOverrideName) {
+    classDef.superclass!.name = maybeOverrideName;
+    classDef.name = `s_${classDef.name}`;
+    return;
+  }
+
+  const maybeOverrideAll = importConfig['*'];
+  if (maybeOverrideAll) {
+    classDef.superclass!.name = maybeOverrideAll(name);
+    classDef.name = `s_${classDef.name}`;
+    return;
+  }
 }
