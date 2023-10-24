bfc274dfe9ef94e22d8bb31c59588c4ac093d42b
Matt Walker <matt.walker@genesis.global>
2023-10-24 08:38:40 +0100

    chore: cleanup project after debugging FUI-1528

--- packages/core/analyzer-import-alias-plugin/src/dev.ts
+++ packages/core/analyzer-import-alias-plugin/src/dev.ts
@@ -1,27 +1,14 @@
 import fs from 'fs';
-import ts from 'typescript';
-// import ts from '../../../../../../../../../../../tmp/cem-plugin-template/node_modules/typescript/lib/typescript.js';
 import { create } from '@custom-elements-manifest/analyzer/src/create.js';
-import myPlugin from './index.js';
+import ts from 'typescript';
+import importAliasPlugin from './index.js';
 
-console.log(`ts.version: ${ts.version} in lib`);
+const defaultCode = fs.readFileSync('fixtures/default/sourcecode/default.js').toString();
+const superclassCode = fs.readFileSync('fixtures/default/sourcecode/superclass.js').toString();
 
-debugger;
+const modules = [
+  ts.createSourceFile('my-element.js', defaultCode, ts.ScriptTarget.ES2021, true),
+  ts.createSourceFile('super-class.js', superclassCode, ts.ScriptTarget.ES2021, true),
+];
 
-// const code = fs.readFileSync('fixtures/default/sourcecode/default.js').toString();
-const code = `
-export class MyElement extends HTMLElement {
-  /**
-   * @foo Some custom information!
-   */
-  someField = '';
-}
-`;
-console.log(code);
-
-const modules = [ts.createSourceFile('my-element.js', code, ts.ScriptTarget.ES2015, true)];
-
-console.log(
-  JSON.stringify(create({ modules, plugins: [], context: { dev: true } }), null, 2)
-  // JSON.stringify(create({ modules, plugins: [myPlugin({})], context: { dev: true } }), null, 2)
-);
+console.log(JSON.stringify(create({ modules, plugins: [importAliasPlugin({})] }), null, 2));
--- packages/core/analyzer-import-alias-plugin/src/index.ts
+++ packages/core/analyzer-import-alias-plugin/src/index.ts
@@ -11,16 +11,44 @@ export type ImportAliasPluginOptions = {
   };
 };
 
-export default function importAliasPlugin(
-  config: ImportAliasPluginOptions
-): Plugin & { name: string } {
+export default function importAliasPlugin(config: ImportAliasPluginOptions): Plugin {
   return {
     name: 'my-plugin',
     // Runs for all modules in a project, before continuing to the `analyzePhase`
     collectPhase({ ts, node, context }) {},
     // Runs for each module
-    analyzePhase({ ts, node, moduleDoc, context }) {},
-    // Runs for each module, after analyzing, all information about your module should now be available
+    analyzePhase({ ts, node, moduleDoc, context }) {
+      // Runs for each module, after analyzing, all information about your module should now be available
+      switch (node.kind) {
+        case ts.SyntaxKind.ClassDeclaration:
+          const className = (node as any).name.getText();
+          (node as any)?.jsDoc?.forEach((jsDoc: any) => {
+            jsDoc.tags?.forEach((tag: any) => {
+              if (tag.tagName.getText() === 'inheritance') {
+                const maybeMatches = [...tag.comment.match(/(\w+)\s+->\s+(\w+)/)];
+                const matchLen = 3;
+                if (maybeMatches.length === matchLen) {
+                  const [_, superclassName, subclassName] = maybeMatches;
+                  const classDeclaration = (moduleDoc as any)?.declarations.find(
+                    (declaration: any) => declaration.name === className,
+                  );
+                  const exportDeclaration = (moduleDoc as any)?.exports.find(
+                    (exp: any) => exp.name === className,
+                  );
+                  debugger;
+                  if (classDeclaration?.superclass?.name && exportDeclaration.declaration?.name) {
+                    classDeclaration.superclass.name = superclassName;
+                    classDeclaration.name = subclassName;
+                    exportDeclaration.name = subclassName;
+                    exportDeclaration.declaration.name = subclassName;
+                  }
+                  debugger;
+                }
+              }
+            });
+          });
+      }
+    },
     moduleLinkPhase({ moduleDoc, context }) {},
     // Runs after modules have been parsed and after post-processing
     packageLinkPhase({ customElementsManifest, context }) {},
