20fafb5b1ca6586838140b85b4efac1fafc745d6
Matt Walker <matt.walker@genesis.global>
2023-10-31 16:48:18 +0000

    test: finish test cases FUI-1528

--- packages/core/analyzer-import-alias-plugin/test/plugin.test.ts
+++ packages/core/analyzer-import-alias-plugin/test/plugin.test.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import { Plugin } from '@custom-elements-manifest/analyzer';
+import type { ClassMember } from 'custom-elements-manifest';
 import ts from 'typescript';
 import importAliasPlugin, { ImportAliasPluginOptions } from '../src';
 import { getAnalyzerCreateHarness } from './analyzer-create';
@@ -11,7 +12,7 @@ import { baseCase } from './fixtures/default/unittest';
 
 const buildTestCase = async (
   config: ImportAliasPluginOptions,
-  phaseOverrides: Partial<Plugin> = {},
+  _phaseOverrides: Partial<Plugin> = {},
 ) => {
   const baseFilePath = '/test/fixtures/default/sourcecode/default.js';
   const anotherFilePath = '/test/fixtures/default/sourcecode/another.js';
@@ -34,6 +35,33 @@ const buildTestCase = async (
   });
 };
 
+const getCorrectTestAssersion = () => {
+  const MISSNG_INHERITED_PROPERTY: ClassMember = {
+    default: "'bar'",
+    inheritedFrom: {
+      module: 'super-class.js',
+      name: 'MyElement',
+    },
+    kind: 'field',
+    name: 'foo',
+    type: {
+      text: 'string',
+    },
+  };
+
+  const expected = structuredClone(baseCase);
+  expected.modules.forEach(
+    (module) =>
+      module.declarations?.forEach(
+        (declaration) =>
+          declaration.kind === 'class' &&
+          declaration.name !== 'AnotherElement' &&
+          declaration?.members?.push(MISSNG_INHERITED_PROPERTY),
+      ),
+  );
+  return expected;
+};
+
 describe('when using no parameters', () => {
   it('produces the base-case manifest where the superclass is ignored', async () => {
     const res = await buildTestCase({});
@@ -41,25 +69,46 @@ describe('when using no parameters', () => {
   });
 });
 
-// describe('when transforming an import not matching any import', () => {
-// it('produces the base-case manifest where the superclass is ignored', async () => {
-// const res = await buildTestCase({ noMatch: {} });
-// expect(res).toEqual(baseCase);
-// });
-// });
-//
-// describe('when transforming an import not matching any override setting', () => {
-// it('produces the base-case manifest where the superclass is ignored', async () => {
-// const res = await buildTestCase({ ['my-library']: { override: { noMatch: 'null' } } });
-// expect(res).toEqual(baseCase);
-// });
-// });
-//
-// describe('when transforming an import using a specific override', () => {
-// it('the matched override is changed and any child class is prepended with NAMESPACE_PREFIX', async () => {
-// const res = await buildTestCase({
-// ['my-library']: { override: { ParentElement: 'MyElement' } },
-// });
-// expect(res).toEqual(baseCase);
-// });
-// });
+describe('when transforming an import not matching any import', () => {
+  it('produces the base-case manifest where the superclass is ignored', async () => {
+    const res = await buildTestCase({ noMatch: {} });
+    expect(res).toEqual(baseCase);
+  });
+});
+
+describe('when transforming an import not matching any override setting', () => {
+  it('produces the base-case manifest where the superclass is ignored', async () => {
+    const res = await buildTestCase({ ['my-library']: { override: { noMatch: 'null' } } });
+    expect(res).toEqual(baseCase);
+  });
+});
+
+describe('when transforming an import using a specific override to allow it to reference the superclass manifest', () => {
+  it('the produced manifest has the correct inherited information', async () => {
+    const res = await buildTestCase({
+      ['my-library']: { override: { ParentElement: 'MyElement' } },
+    });
+    expect(res).toEqual(getCorrectTestAssersion());
+  });
+});
+
+describe('when transforming an import using a transformer function, to allow it to reference the superclass manifest', () => {
+  it('the produced manifest has the correct inherited information', async () => {
+    const res = await buildTestCase({
+      ['my-library']: { '*': (name) => name.replace('Parent', 'My') },
+    });
+    expect(res).toEqual(getCorrectTestAssersion());
+  });
+});
+
+describe('config override takes precedent over the transformer function', () => {
+  it('the produced manifest has the correct inherited information', async () => {
+    const res = await buildTestCase({
+      ['my-library']: {
+        override: { ParentElement: 'MyElement' },
+        '*': (name) => name.replace('Parent', 'Base'),
+      },
+    });
+    expect(res).toEqual(getCorrectTestAssersion());
+  });
+});
